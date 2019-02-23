55f7e161cc592cc6cd9e1e5fde2f1d2ba04d5121
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-02-23 10:44:02 +0530

    Update Home Contoller

--- src/main/java/com/javapoint/HomeController.java
+++ src/main/java/com/javapoint/HomeController.java
@@ -3,9 +3,15 @@ package com.javapoint;
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.bind.annotation.RestController;  
 @RestController  
-public class HomeController {  
+public class HomeController {
+	public String name;
     @RequestMapping("/hello")  
     public String hello(){  
-        return"Hello!";  
-    }  
+        return this.name == null ? "Name isn't set": this.name;
+    }
+    @RequestMapping(value="/homepage")
+    public void StoreName(String name)
+    {
+    	this.name=name;
+    }
 }
